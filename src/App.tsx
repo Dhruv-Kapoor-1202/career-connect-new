11c65ec2dc883235d3226663e4e64de93b054708
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-20 01:00:25 +0530

    feat: Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Enroll from "./pages/Enroll";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { buttonVariants } from "../ui/button";
+import { Menu } from "lucide-react";
+import { ModeToggle } from "../mode-toggle";
+import { Link } from "react-router-dom";
+
+interface RouteProps {
+  to: string;
+  label: string;
+}
+
+const routeList: RouteProps[] = [
+  {
+    to: "/course",
+    label: "Course",
+  },
+  {
+    to: "/about",
+    label: "About",
+  },
+  {
+    to: "/contact",
+    label: "Contact",
+  }
+];
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <header className="sticky border-b-[1px] top-0 z-40 w-full bg-white dark:border-b-slate-700 dark:bg-background">
+      <NavigationMenu className="mx-auto">
+        <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between ">
+          <NavigationMenuItem className="font-bold flex mr-auto">
+            <Link
+              to="/"
+              className="ml-2 font-bold text-xl flex"
+            >
+              CareerConnect
+            </Link>
+          </NavigationMenuItem>
+
+          {/* mobile */}
+          <span className="flex md:hidden">
+            <ModeToggle />
+
+            <Sheet
+              open={isOpen}
+              onOpenChange={setIsOpen}
+            >
+              <SheetTrigger className="px-2">
+                <Menu
+                  className="flex md:hidden h-5 w-5"
+                  onClick={() => setIsOpen(true)}
+                >
+                  <span className="sr-only">Menu Icon</span>
+                </Menu>
+              </SheetTrigger>
+
+              <SheetContent side={"left"}>
+                <SheetHeader>
+                  <SheetTitle className="font-bold text-xl">
+                    CareerConnect
+                  </SheetTitle>
+                </SheetHeader>
+                <nav className="flex flex-col justify-center items-center gap-2 mt-4">
+                  {routeList.map(({ to, label }: RouteProps) => (
+                    <Link
+                      key={label}
+                      to={to}
+                      onClick={() => setIsOpen(false)}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      {label}
+                    </Link>
+                  ))}
+                  <Link
+                    to="/enroll"
+                    className={`w-[110px] border ${buttonVariants({
+                      variant: "default",
+                    })}`}
+                  >
+                    Enroll Now
+                  </Link>
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </span>
+
+          {/* desktop */}
+          <nav className="hidden md:flex gap-2">
+            {routeList.map((route: RouteProps, i) => (
+              <Link
+                to={route.to}
+                key={i}
+                className={`text-[17px] ${buttonVariants({
+                  variant: "ghost",
+                })}`}
+              >
+                {route.label}
+              </Link>
+            ))}
+          </nav>
+
+          <div className="hidden md:flex gap-2">
+            <ModeToggle />
+
+            <Link
+              to="/enroll"
+              className={`border ${buttonVariants({ variant: "default" })}`}
+            >
+              Enroll Now
+            </Link>
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
