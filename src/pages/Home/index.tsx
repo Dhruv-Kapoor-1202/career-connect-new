4ef8bcd5c928abb63fc42ce02aadb4ea00981e63
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-20 13:27:37 +0530

    feat: Add MeetYourCaptain Component to Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,16 @@
 import { AboutCC } from "@/components/AboutCC";
 import { Blueprint } from "@/components/Blueprint";
 import { CtaOne } from "@/components/CtaOne";
+import Hero from "@/components/Hero";
+import { MeetYourCaptain } from "@/components/MeetYourCaptain";
 
 const Home = () => {
   return (
     <>
-      Home
+      <Hero />
       <AboutCC />
       <Blueprint />
+      <MeetYourCaptain />
       <CtaOne />
     </>
   )
