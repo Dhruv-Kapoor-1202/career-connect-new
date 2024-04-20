725068dd67602770e9d5a9e0b7f56bd2e87c8163
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-20 10:50:30 +0530

    feat: Add CtaOne Component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Blueprint } from "@/components/Blueprint";
+import { CtaOne } from "@/components/CtaOne";
 
 const Home = () => {
   return (
     <>
       Home
       <Blueprint />
+      <CtaOne />
     </>
   )
 };
