164666cdba7f9f124b3afaf3e181a733ca1f970e
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-20 22:21:59 +0530

    fix: Update CourseHeader Styles

--- src/components/CourseHeader/index.tsx
+++ src/components/CourseHeader/index.tsx
@@ -7,17 +7,17 @@ const CourseHeader = () => {
   return (
     <section className='container text-center py-24'>
 
-      <header className="bg-primary  flex items-center justify-between rounded-[30px] w-full overflow-hidden">
+      <header className="bg-primary  flex flex-col-reverse md:flex-row items-start justify-start md:items-center md:justify-between rounded-[30px] w-full overflow-hidden">
         {/* Main Content Container */}
-        <div className="flex items-center p-11 w-full">
-          <div className="text-white text-lg space-y-2 flex flex-col gap-6 justify-start items-start">
+        <div className="flex items-start justify-start py-11 px-7 sm:px-11 w-full">
+          <div className="text-white text-start text-lg space-y-2 flex flex-col gap-6 justify-start items-start">
             {/* Course Details */}
-            <h1 className="text-3xl md:text-4xl font-bold">Career Accelerator Program</h1>
+            <h1 className="text-3xl md:text-4xl font-bold text-start">Career Accelerator Program</h1>
 
             <div className='flex flex-col gap-4 justify-start items-start'>
-              <p className='text-xl'>Duration: 12 Weeks</p>
-              <p className='text-xl'>Availability: Starts May 1st</p>
-              <p className='text-xl'>Price: $499</p>
+              <p className='text-lg md:text-xl'>Duration: 12 Weeks</p>
+              <p className='text-lg md:text-xl'>Availability: Starts May 1st</p>
+              <p className='text-lg md:text-xl'>Price: $499</p>
             </div>
 
             <Link
@@ -28,7 +28,7 @@ const CourseHeader = () => {
             </Link>
           </div>
         </div>
-        <img src={Pic} alt="Career Accelerator Program" className="object-cover w-full max-h-[450px]" />
+        <img src={Pic} alt="Career Accelerator Program" className="object-cover w-full max-h-[500px] lg:max-h-[450px]" />
       </header>
     </section>
   );
