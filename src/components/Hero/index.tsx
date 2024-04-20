c1fa98e83c349bc5de82b968fba8212e759e2d46
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-20 14:14:57 +0530

    feat: Add Hero Section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,35 +2,49 @@
 import { Link } from "react-router-dom"
 import { buttonVariants } from "../ui/button"
 
+import Pic from "../../assets/pics/Pic-12.jpg"
+
+
 export default function Hero() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 relative">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center space-y-6 text-center">
           <div className="space-y-4 flex flex-col justify-center items-center gap-4">
-            <h1 className="text-4xl max-w-[15ch] font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
+            <h1 className="text-4xl max-w-[15ch] font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl ">
               Holding Your Hand From Preparation To Placement
             </h1>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+            {/* <p className="mx-auto max-w-[700px] text-gray-50 md:text-xl dark:text-gray-400"> */}
+            <p className="mx-auto max-w-[700px]  md:text-xl dark:text-gray-400">
               Learn the latest technologies and frameworks to build stunning web applications. Enroll in our
               comprehensive course today.
             </p>
           </div>
           <div className="flex flex-col gap-4 sm:flex-row">
             <Link
-              to="/"
-              className={`${buttonVariants({ variant: "default" })}`}
+              to="/enroll"
+              className={`${buttonVariants({ variant: "default", size: 'lg' })}`}
             >
               Enroll Now
             </Link>
             <Link
-              to="/"
-              className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200  bg-white px-6 py-2 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800  dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+              to="/about"
+              className={`${buttonVariants({ variant: "outline", size: 'lg' })}`}
             >
               Learn More
             </Link>
           </div>
         </div>
+      </div>
+      {/* <div className="hidden bg-muted sm:block absolute inset-0 -z-10 overflow-hidden"> */}
+      <div className=" bg-muted block absolute inset-0 -z-10 overflow-hidden">
+        <img
+          src={Pic}
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover blur-sm opacity-45 dark:opacity-100  dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </section>
   )
