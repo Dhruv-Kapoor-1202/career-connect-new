602c73305578eab236d83a3cb479e591cb34cd0f
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-21 21:41:36 +0530

    fix: Add Ma'am changes 4???

--- src/components/MeetYourCaptain/index.tsx
+++ src/components/MeetYourCaptain/index.tsx
@@ -19,7 +19,12 @@ export const MeetYourCaptain = () => {
             Meet the coach who keeps it real. With certifications from the National Skills Development Corporation and Career Development Alliance-USA.
           </p>
           <p className="text-muted-foreground text-xl mt-4 mb-8 ">
-            I’m not just here to guide your career, I’m here to turn it into a blockbuster. 7+ Years of Experience, 5000+ Wins <br/>
+            I’m not just here to guide your career, I’m here to turn it into a blockbuster.
+          </p>
+          <p className="text-muted-foreground text-xl mt-4 mb-8 ">
+            7+ Years of Experience, 5000+ Wins
+          </p>
+          <p className="text-muted-foreground text-xl mt-4 mb-8 ">
             Let’s make your journey as impressive as your goals!
           </p>
 
