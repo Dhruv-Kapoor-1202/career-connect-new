7ca80235d3b965b749c97bbdef4420056491539e
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-21 16:58:14 +0530

    feat: Add About Page

---
+++ src/components/AboutHero/index.tsx
@@ -0,0 +1,40 @@
+import Pic from "@/assets/ContactUs.jpg"
+
+export const AboutHero = () => {
+  return (
+    <header className="container py-24 sm:py-32 flex flex-col justify-start items-center">
+      <h2 className="text-3xl md:text-4xl font-bold text-center mb-12 max-w-[30ch]">
+        Empowering Your{" "}
+        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+          Career Journey{" "}
+        </span>
+        with{" "}
+        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+          Education
+        </span>
+      </h2>
+      <div className="text-center mb-8 relative overflow-hidden w-full h-[500px] flex flex-col justify-center items-center rounded-[30px]">
+        <h3 className="text-4xl md:text-5xl font-extrabold text-center mb-12 text-white">
+          Career{" "}
+          <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+            Connect
+          </span>
+        </h3>
+        <p className="text-white mx-auto max-w-[700px] sm:text-xl">Your one-stop platform for skill development.</p>
+        <img
+          src={Pic}
+          alt="CareerConnect - Education Platform"
+          className="object-cover h-[450px] w-full  md:h-[600px] lg:h-[600px]  shrink shadow-md absolute inset-0 -z-10 blur-sm brightness-[0.6]  dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      {/* <div className="grid lg:grid-cols-[1fr,1fr] gap-8 place-items-center"> */}
+
+      {/* <img
+        src={Pic}
+        alt="CareerConnect - Education Platform"
+        className="object-cover h-[450px] w-full  md:h-[600px] lg:h-[600px] rounded-[30px] shrink shadow-md absolute inset-0"
+      /> */}
+      {/* </div> */}
+    </header>
+  )
+};
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,6 +1,36 @@
+import { AboutCta } from '@/components/AboutCta';
+import { AboutHero } from '@/components/AboutHero';
+import { AboutStory } from '@/components/AboutStory';
+import { AboutVision } from '@/components/AboutVision';
+import { Testimonials } from '@/components/Testimonials';
+
 const About = () => {
   return (
-    <>About</>
+    <>
+      {/* Header Section */}
+      <AboutHero />
+
+      {/* Story Section */}
+      <AboutStory />
+
+      {/* Primary CTA */}
+      <AboutCta />
+
+      {/* Testimonials */}
+      <Testimonials />
+
+
+
+      {/* Secondary CTA */}
+      {/* <div className="text-center py-8">
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400">
+          Get Started Today
+        </button>
+      </div> */}
+
+      {/* Vision Section */}
+      <AboutVision />
+    </>
   )
 };
 
