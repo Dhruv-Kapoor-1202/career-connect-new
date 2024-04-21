288ed1506cf97b128f93d1facdbedb83a88c768a
Hemanya09 <hemanya1721.be21@chitkara.edu.in>
2024-04-21 21:02:23 +0530

    changes in cta one

--- src/components/CtaOne/index.tsx
+++ src/components/CtaOne/index.tsx
@@ -5,17 +5,18 @@ export const CtaOne = () => {
   return (
     <section
       id="ctaOne"
-      className="bg-muted/50 py-16 my-24 sm:my-32"
+      className="bg-secondary py-16 my-24 sm:my-32"
     >
       <div className="container lg:grid lg:grid-cols-2 place-items-center">
         <div className="lg:col-start-1">
           {/* <h2 className="text-3xl md:text-4xl font-bold "> */}
-          <h2 className="text-3xl md:text-4xl font-bold mb-8 lg:mb-0">
-            Follow Our step by step approach to make
+          <h2 className="text-2xl md:text-3xl font-bold mb-8 lg:mb-0">
+            Make
             Your Job Search{" "}
             <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-              Chaos Free
+              Chaos Free {" "}
             </span>
+            Today
           </h2>
           {/* <p className="text-muted-foreground text-xl mt-4 mb-8 lg:mb-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
