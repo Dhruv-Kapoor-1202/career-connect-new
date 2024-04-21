7cc790e7e6c6bfb5f7146b89468641ef924d35b3
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-04-21 21:42:58 +0530

    fix: Add me change 1.

--- src/components/MeetYourCaptain/index.tsx
+++ src/components/MeetYourCaptain/index.tsx
@@ -8,7 +8,7 @@ export const MeetYourCaptain = () => {
       <div className="grid lg:grid-cols-[1fr,1fr] gap-8 place-items-center">
 
         <div>
-          <h2 className="text-3xl md:text-4xl font-bold">
+          <h2 className="text-3xl md:text-4xl font-bold mb-12">
             Meet Your{" "}
             <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
               Career Captain
