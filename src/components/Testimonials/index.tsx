9530676900866e50a4630c601e69009eeff33aca
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-07-19 17:54:10 +0530

    fix: add testimonial images

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -7,20 +7,36 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import DC from "../../assets/testimonials/DC.png";
+import DJ from "../../assets/testimonials/DC.jpg";
+import DB from "../../assets/testimonials/DB.jpg";
+import RK from "../../assets/testimonials/RK.jpg";
+import JN from "../../assets/testimonials/JIELTS.jpg";
+import NP from "../../assets/testimonials/pic2.jpg";
+
 interface TestimonialProps {
   image: string;
   name: string;
   userName: string;
   comment: string;
+  img: string;
 }
 
 const testimonials: TestimonialProps[] = [
+  {
+    image: "DB",
+    name: "Diotima Bose",
+    userName: "@Diotima_Bose3",
+    comment: "",
+    img: DB,
+  },
   {
     image: "DC",
     name: "Dinesh Choudhary",
     userName: "@dinesh_choudhary",
     comment:
       "We learned something new in every lecture. The passion for teaching and expertise in the subject made it easy for us to understand the subject.",
+    img: DC,
   },
   {
     image: "DJ",
@@ -28,6 +44,7 @@ const testimonials: TestimonialProps[] = [
     userName: "@dikshant.jagtap",
     comment:
       "I would like to thank the wonderful faculty for guiding us on communication skills behavioural skills and many more things. It made such a difference in my attitude.",
+    img: DJ,
   },
 
   {
@@ -36,13 +53,15 @@ const testimonials: TestimonialProps[] = [
     userName: "@rushi_kalane2",
     comment:
       "Journey from the basics of communication to personal interview and GD all you covered was fantastic. I really enjoyed your sessions. What impressed me the most was your willingness to help the students.",
+    img: RK,
   },
   {
-    image: "SS",
-    name: "Sunil Srivastav",
-    userName: "@sunil_sri21",
+    image: "NP",
+    name: "Naila Parween",
+    userName: "@pnaila",
     comment:
       "Learning communication skills from the mentor was a pleasent and favourable experience for me. One thing I want to mention here is that earlier I used to think that since I have no professional experience, I can never convince my recruiter on certain things but you Instilled the confidence in me to use my academic achievments to better present my capabilities.",
+    img: NP,
   },
   {
     image: "JN",
@@ -50,15 +69,10 @@ const testimonials: TestimonialProps[] = [
     userName: "@jasleen_4",
     comment:
       "Thankyou so much for giving guidance to me because of you only I got such good results in IELTS.. Thankyou so much ma'am.",
-  },
-  {
-    image: "DB",
-    name: "Diotima Bose",
-    userName: "@Diotima_Bose3",
-    comment:
-      "Thankyou soo much ma'am for giving me the appropriate guidance since the begining till my exams <3",
+    img: JN,
   },
 ];
+// "Thankyou soo much ma'am for giving me the appropriate guidance since the begining till my exams <3",
 
 export const Testimonials = () => {
   return (
@@ -79,11 +93,10 @@ export const Testimonials = () => {
 
       <div className="grid md:grid-cols-2 lg:grid-cols-4 sm:block columns-2  lg:columns-3 lg:gap-6 mx-auto space-y-4 lg:space-y-6">
         {testimonials.map(
-          ({ image, name, userName, comment }: TestimonialProps) => (
+          ({ image, name, userName, comment, img }: TestimonialProps) => (
             <Card
               key={userName}
-              className="max-w-md md:break-inside-avoid overflow-hidden hover:bg-secondary/50 duration-300"
-            >
+              className="max-w-md md:break-inside-avoid overflow-hidden hover:bg-secondary/50 duration-300">
               <CardHeader className="flex flex-row items-center gap-4 pb-2">
                 <Avatar>
                   <AvatarImage alt="" src={image} />
@@ -96,7 +109,14 @@ export const Testimonials = () => {
                 </div>
               </CardHeader>
 
-              <CardContent>{comment}</CardContent>
+              <CardContent>
+                <span className="sr-only hidden">{comment}</span>
+                <img
+                  src={img}
+                  alt="testimonial image"
+                  className="rounded-sm border border-border mt-2"
+                />
+              </CardContent>
             </Card>
           )
         )}
